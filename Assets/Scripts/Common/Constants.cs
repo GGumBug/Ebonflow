873dc2637b339bc2928646d6437e378985f7f6e2
GGumBug <119824540+GGumBug@users.noreply.github.com>
2025-02-27 10:40:35 +0900

    방향 벡터 배열 전역으로 이동

--- Assets/Scripts/Common/Constants.cs
+++ Assets/Scripts/Common/Constants.cs
@@ -1,4 +1,22 @@
+using UnityEngine;
+
 public static class Constants
 {
     public const string LOADING_SCENE_NAME = "LoadingScene";
+
+    public static readonly Vector2Int[] DIAGONAL_DIRECTIONS = new Vector2Int[]
+    {
+        new Vector2Int(1, 1),
+        new Vector2Int(-1, 1),
+        new Vector2Int(-1, -1),
+        new Vector2Int(1, -1)
+    };
+
+    public static readonly Vector2Int[] ORTHOGONAL_DIRECTIONS = new Vector2Int[]
+    {
+        new Vector2Int(0, 1),
+        new Vector2Int(1, 0),
+        new Vector2Int(0, -1),
+        new Vector2Int(-1, 0)
+    };
 }
