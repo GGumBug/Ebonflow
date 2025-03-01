d8070c61791232136343878df34811def0d871f6
GGumBug <119824540+GGumBug@users.noreply.github.com>
2025-03-01 22:20:00 +0900

    RangeDetector 클래스 추가

---
+++ Assets/Scripts/Unit/RangeDetector.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class RangeDetector : MonoBehaviour
+{
+    [Header("Detection Settings")]
+    [Tooltip("월드 단위의 감지 반지름입니다.")]
+    [SerializeField] private float detectionRadius = 5f;
+
+    [Tooltip("감지할 오브젝트의 레이어 마스크입니다.")]
+    [SerializeField] private LayerMask detectionLayer;
+
+    private Collider2D[] GetObjectsInRange()
+    {
+        return Physics2D.OverlapCircleAll(transform.position, detectionRadius, detectionLayer);
+    }
+
+    public bool IsObjectInRange()
+    {
+        var colliders = GetObjectsInRange();
+        foreach (var col in colliders)
+        {
+            if (col.gameObject != gameObject)
+                return true;
+        }
+
+        return false;
+    }
+
+    public GameObject GetClosestObjectInRange()
+    {
+        Collider2D[] colliders = GetObjectsInRange();
+        GameObject closestObject = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (var col in colliders)
+        {
+            if (col.gameObject == gameObject)
+                continue;
+
+            float distance = Vector2.Distance(transform.position, col.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestObject = col.gameObject;
+            }
+        }
+
+        return closestObject;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
