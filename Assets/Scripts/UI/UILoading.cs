2ffe1fcdd299d70e49d39ff3d836cb45ac749bce
GGumBug <119824540+GGumBug@users.noreply.github.com>
2025-06-16 21:57:39 +0900

    로딩 ui 개선

--- Assets/Scripts/UI/UILoading.cs
+++ Assets/Scripts/UI/UILoading.cs
@@ -1,8 +1,8 @@
-using Cysharp.Threading.Tasks;
-using DG.Tweening;
-using TMPro;
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
 using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 public class UILoading : MonoBehaviour
 {
@@ -14,7 +14,8 @@ public class UILoading : MonoBehaviour
     {
         canvasGroup = GetComponent<CanvasGroup>();
         txtLoadingPercent = GetComponentInChildren<TextMeshProUGUI>();
-        SceneLoadManager.Instance.OnSceneLoadComplete += () => LoadingEndAction();
+
+        SceneLoadManager.Instance.OnSceneLoadComplete += LoadingEndAction;
     }
 
     private async void OnEnable()
@@ -22,18 +23,30 @@ public class UILoading : MonoBehaviour
         await LoadingStartAnimation();
     }
 
+    private void OnDisable()
+    {
+        DOTween.Kill(canvasGroup); // 안전하게 Tween 제거
+    }
+
+    private void OnDestroy()
+    {
+        SceneLoadManager.Instance.OnSceneLoadComplete -= LoadingEndAction;
+    }
+
     public async UniTask LoadingStartAnimation()
     {
-        await canvasGroup.DOFade(1, fadeDuration).OnComplete(async () => await SceneLoadManager.Instance.ActivateTargetScene());
+        await canvasGroup.DOFade(1, fadeDuration);
+        await SceneLoadManager.Instance.ActivateTargetScene();
     }
 
     public void UpdateProgress(float value)
     {
-        txtLoadingPercent.text = value + "%";
+        txtLoadingPercent.text = $"{Mathf.FloorToInt(value)}%";
     }
 
     private async void LoadingEndAction()
     {
-        await canvasGroup.DOFade(0, fadeDuration).OnComplete(async () => await SceneManager.UnloadSceneAsync(Constants.LOADING_SCENE_NAME));
+        await canvasGroup.DOFade(0, fadeDuration);
+        await SceneManager.UnloadSceneAsync(Constants.LOADING_SCENE_NAME);
     }
 }
