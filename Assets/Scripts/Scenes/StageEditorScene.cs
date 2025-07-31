269ef2413559a8104f635f5ddcdef5701252a608
GGumBug <119824540+GGumBug@users.noreply.github.com>
2025-07-31 16:46:22 +0900

    StageEditorScene 생성

---
+++ Assets/Scripts/Scenes/StageEditorScene.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class StageEditorScene : MonoBehaviour
+{
+    [SerializeField] private Vector2Int _gridTopRight;
+    [SerializeField] private Vector2Int _gridBottomLeft;
+}
